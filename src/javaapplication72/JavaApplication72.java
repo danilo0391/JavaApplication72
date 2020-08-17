d826fc34531d6b81af4b58b4f9c81ab4dff00f5b
Danilo Pereira <danilo_tw@hotmail.com>
2020-08-17 16:03:14 +0100

    Second-branch illustrating

--- src/javaapplication72/JavaApplication72.java
+++ src/javaapplication72/JavaApplication72.java
@@ -20,23 +20,6 @@ public class JavaApplication72 {
      */
     public static void main(String[] args) {
 
-        Person p1 = new Person("Danilo", 35, 1.77);
-        p1.run();
 
-        Person p2 = new Person("Peter", 35, 1.77);
-        p2.run();
-
-        Car c1 = new Car("Fiat", "Red", 5, "Diesel");
-        c1.newCar();
-
-        Car c2 = new Car("Volvo", "Black", 3, "Petrol");
-        c2.newCar();
-
-        Dog d1 = new Dog("Dalmata", "Google", 10);
-        d1.myDog();
-        
-        Dog d2 = new Dog("Husk", "Android", 5);
-        d2.myDog();
-    }
 
 }
